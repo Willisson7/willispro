5fe5751c68333edc6a6ab88470496bc23883d225
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-06-17 11:41:21 -0700

    Worked in the nav bat. Added side Willis Portfolio

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,11 +4,11 @@ import '../styles/nav.css';
 function Navbar() {
   return (
     <header className="navHeader">
-      <div className="pro">WillisPro</div>
+      <div className="pro">Willis Portfolio</div>
      <ul>
       <li><Link className="navlink" to ="/">Home</Link></li>
       <li><Link className="navlink" to="/About">About</Link></li>
-      <li><Link className="navlink" to="/Portfolio">Portfolio</Link></li>
+      <li><Link className="navlink" to="/Portfolio">Projects</Link></li>
       <li><Link className="navlink" to="/Contact">Contact</Link></li>
       <li><Link className="navlink" to="/Resume">Resume</Link></li>
      </ul>
